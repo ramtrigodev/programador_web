87fb64f663fa0325cddcbfe35f50a1ef75c9f33c
Ramon Trigo <ramtrigocloud@gmail.com>
2023-02-02 15:24:03 -0300

    evento_load

---
+++ java_script/load.js
@@ -0,0 +1,7 @@
+function carreguei() {
+  document.getElementById("log").innerHTML += "carreguei<br>";
+    
+}
+function redimensionei() {
+    document.getElementById("log").innerHTML +="redimensionei<br>";
+}
